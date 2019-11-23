aa90780410d203226a185cbc01d906da237283a3
Marek Zabialowicz <marekzabialowicz@gmail.com>
2019-11-23 19:12:40 +0100

    songs

--- New/Program.py
+++ New/Program.py
@@ -1,14 +1,25 @@
 import WavReader
 import numpy
 import TempoDetector
+import Song
 
 BANDLIMITS = [0, 200, 400, 800, 1600, 3200]
 MAXFREQ = 44100
 sampleSize = numpy.floor(2.2 * MAXFREQ)
 drawPlots = False
 
-song = WavReader.read('pop\\1.wav')
+songs = {
+    Song.Song('120', 120, 'songs\\test\\120.wav'),
+    Song.Song('100', 100, 'songs\\test\\100.wav'),
+    Song.Song('1-propinan_de_melyor', 115, 'songs\\classical\\1-propinan_de_melyor.wav'),
+    Song.Song('18-dolce_amoroso_focho', 140, 'songs\\classical\\18-dolce_amoroso_focho.wav'),
+    Song.Song('1-soul_print', 130, 'songs\\metal\\1-soul_print.wav'),
+    Song.Song('11-divine_toy', 160, 'songs\\metal\\11-divine_toy.wav'),
+    Song.Song('6-trugnale_marko', 150, 'songs\\world\\6-trugnale_marko.wav'),
+    Song.Song('8-daldalar_tamzara', 170, 'songs\\world\\8-daldalar_tamzara.wav'),
+}
 
-song_bpm = TempoDetector.detect(song, False)
-print("Song bpm: ")
-print(song_bpm)
+for song in songs:
+    signal = WavReader.read(song.filepath)
+    bpm = TempoDetector.detect(signal, False)
+    print(f"Detected in {song.name} song's bpm: {bpm} should be {song.bpm}")
---
+++ New/Song.py
@@ -0,0 +1,9 @@
+class Song:
+    name = "name"
+    bpm = 120,
+    filepath = "test"
+
+    def __init__(self, name, bpm, filepath):
+        self.name = name
+        self.bpm = bpm
+        self.filepath = filepath
