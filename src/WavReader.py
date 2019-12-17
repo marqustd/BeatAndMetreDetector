28e07f7b993532e88a4efeae1272deace9003a35
Marek <marekzabialowicz@gmail.com>
2019-12-17 18:56:24 +0100

    clean up

--- src/WavReader.py
+++
@@ -1,8 +0,0 @@
-import scipy.io.wavfile
-import numpy
-
-
-def read(filename):
-    sample_freq, data = scipy.io.wavfile.read(filename)
-    signal = numpy.frombuffer(data, numpy.int16)
-    return signal, sample_freq
