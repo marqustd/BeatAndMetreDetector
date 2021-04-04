46c7f0c9a2a9704e05a44f680219f54188544665
Marek Żabiałowicz <marekzabialowicz@gmail.com>
2021-04-04 15:07:01 +0200

    songs

--- src/songReader.py
+++ src/songReader.py
@@ -2,7 +2,16 @@ import scipy.io.wavfile
 import numpy as np
 
 
-def read_song(filename):
+def read_song(filename: str):
+    """Reads song and returns it as a numpy array witg sampling frequency
+
+    Args:
+        filename (string): Filepath
+
+    Returns:
+        numpy array: data
+        int: sampling frequency
+    """
     sample_freq, data = scipy.io.wavfile.read(filename)
     signal = np.frombuffer(data, np.int16)
     return signal, sample_freq
