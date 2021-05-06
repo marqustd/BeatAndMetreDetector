bc3c42e5b79e570ae78bcfc2944fef5624c2dc15
Marek Żabiałowicz <marekzabialowicz@gmail.com>
2021-05-06 19:05:13 +0200

    asm

--- src/gainza.py
+++ src/gainza.py
@@ -15,33 +15,31 @@ sample
 songTempo = 92
 songTempo
 
+# %% Calculate beat duration
+beatDurationSec = 60 / songTempo
+beatDurationSample = int(beatDurationSec * samplingFrequency)
+beatDurationSec
+
 # %% target spectrogram
-plt.specgram(sample, Fs=samplingFrequency, NFFT=1024, noverlap=900)
+plt.specgram(sample, Fs=samplingFrequency, NFFT=beatDurationSample)
 plt.show()
 
 # %% Calculate spectrogram
-
-frequencies, times, spectrogram = signal.spectrogram(sample, samplingFrequency)
-
-# %% Calculate beat duration
-beatDurationSec = 60 / songTempo
-beatDurationSec
-
-# %% Calcute time ranges
-timesIndexes = [0]
-for time in np.arange(beatDurationSec, times[-1], beatDurationSec):
-    timesIndexes.append(findLastIndexOfLessOrEqual(times, time))
-
-# %% Divide to beat bins
-beats = np.ndarray((len(timesIndexes), 129))  # todo
-for i in range(len(timesIndexes)-1):
-    timeRange = (spectrogram[:, timesIndexes[i]:timesIndexes[i+1]])
-    beat = np.ndarray(129)
-    transposed = np.transpose(timeRange)
-    for k in range(len(transposed)):
-        beat[k] = sum(transposed[k])
-    beats[i] = beat
-
+frequencies, times, spectrogram = signal.spectrogram(
+    sample, samplingFrequency, nperseg=beatDurationSample)
 
 # %% Calculate AMS
+binsAmount = len(times)
+ams = np.ndarray((binsAmount, binsAmount))
+
+for x in range(binsAmount):
+    thisbin = spectrogram[:, x]
+    for y in range(binsAmount):
+        comparedBin = spectrogram[:, y]
+        ams[y, x] = sum(thisbin-comparedBin)
+
+#%%        
+plt.pcolormesh(ams)
+plt.show()
+
 # %% Calculate BMS
