7e144d2b6efbed8e7888254a17d5201d587af45d
Marek Żabiałowicz <marekzabialowicz@gmail.com>
2021-07-04 20:36:48 +0200

    tests green

--- src/tests/harmonicPercusiveTest.py
+++ src/tests/harmonicPercusiveTest.py
@@ -5,13 +5,29 @@ import numpy as np
 
 
 class Tests(unittest.TestCase):
-    def test_median_filter(self):
+    def test_median_filter_square(self):
         spectrogram = np.array([
             [1, 101, 3],
             [4, 5, 206],
             [7, 8, 9]])
         median_filter(spectrogram, 1,1,3,3)
         self.assertEqual(spectrogram[1,1],7)
+        
+    def test_median_filter_vertical(self):
+        spectrogram = np.array([
+        [1, 9, 3],
+        [19, 8, 3],
+        [7, 206, 1]])
+        median_filter(spectrogram, 1,1,3,0)
+        self.assertEqual(spectrogram[1,1],9)
+
+    def test_median_filter_horizontal(self):
+        spectrogram = np.array([
+        [1, 101, 3],
+        [17, 8, 206],
+        [7, 8, 9]])
+        median_filter(spectrogram, 1,1,0,3)
+        self.assertEqual(spectrogram[1,1],17)
 
 
 if __name__ == '__main__':
